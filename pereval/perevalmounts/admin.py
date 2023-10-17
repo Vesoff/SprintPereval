be33284b0d8266fbca3d59a4f0171ee5d43dc6ef
Vesoff <125479477+Vesoff@users.noreply.github.com>
2023-10-17 03:42:20 +0300

    Admin.site models registration

--- pereval/perevalmounts/admin.py
+++ pereval/perevalmounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import User, Coords, Level, Pereval, Images
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Coords)
+admin.site.register(Level)
+admin.site.register(Pereval)
+admin.site.register(Images)
