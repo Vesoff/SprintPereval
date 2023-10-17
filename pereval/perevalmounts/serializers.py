c31c45900d80a027a14d72f28ee865d60dac21e7
Vesoff <125479477+Vesoff@users.noreply.github.com>
2023-10-17 04:54:33 +0300

    Serializers added

--- pereval/perevalmounts/serializers.py
+++ pereval/perevalmounts/serializers.py
@@ -1,3 +1,39 @@
 from rest_framework import serializers
 from models import User, Coords, Level, Pereval, Images
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = '__all__'
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = '__all__'
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    data = serializers.URLField()
+
+    class Meta:
+        model = Images
+        fields = '__all__'
+
+
+class PerevalSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    coords = CoordsSerializer()
+    level = LevelSerializer(allow_null=True)
+    images = ImagesSerializer(many=True)
+
+    class Meta:
+        model = Pereval
+        fields = '__all__'
