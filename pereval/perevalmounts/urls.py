38ca8f1903dbc7642ca63272906cd873cca33345
Vesoff <125479477+Vesoff@users.noreply.github.com>
2023-10-17 05:13:26 +0300

    Viewsets added + urls router registration

--- pereval/perevalmounts/urls.py
+++ pereval/perevalmounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
+from views import PerevalViewSet
 
 router = routers.DefaultRouter()
-
+router.register(r'pereval', PerevalViewSet, basename='pereval')
 
 urlpatterns = [
     path('submitData/', include(router.urls)),
--- pereval/perevalmounts/views.py
+++ pereval/perevalmounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from serializers import UserSerializer, CoordsSerializer, LevelSerializer, ImagesSerializer, PerevalSerializer
+from models import User, Coords, Level, Images, Pereval
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializers_class = UserSerializer
+
+
+class CoordsViewSet(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializers_class = CoordsSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializers_class = LevelSerializer
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializers_class = ImagesSerializer
+
+
+class PerevalViewSet(viewsets.ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializers_class = PerevalSerializer
