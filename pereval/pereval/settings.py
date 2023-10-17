0b19710977fc63eb36d17c934bd624bac4419245
Vesoff <125479477+Vesoff@users.noreply.github.com>
2023-10-17 03:36:03 +0300

    DRF installation

--- pereval/pereval/settings.py
+++ pereval/pereval/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'perevalmounts'
+    'perevalmounts',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
