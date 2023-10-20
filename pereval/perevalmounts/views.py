bb1a0a40e1309c50e2863f55746438a8292bec41
Vesoff <125479477+Vesoff@users.noreply.github.com>
2023-10-20 22:42:03 +0300

    Changes permition for only new status, notification for update object data state

--- pereval/perevalmounts/views.py
+++ pereval/perevalmounts/views.py
@@ -2,6 +2,7 @@ from .serializers import UserSerializer, CoordsSerializer, LevelSerializer, Imag
 from .models import User, Coords, Level, Images, Pereval
 from rest_framework import viewsets
 import django_filters
+from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -30,3 +31,29 @@ class PerevalViewSet(viewsets.ModelViewSet):
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ["user__email"]
 
+    def partial_update(self, request, *args, **kwargs):
+        record = self.get_object()
+        if record.status == 'new':
+            serializer = PerevalSerializer(record, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'state': '1',
+                        'message': 'Изменения внесены успешно'
+                    }
+                )
+            else:
+                return Response(
+                    {
+                        'state': '0',
+                        'message': serializer.errors
+                    }
+                )
+        else:
+            return Response(
+                {
+                    'state': '0',
+                    'message': f'При данном статусе: {record.get_status_display()}, данные изменить нельзя!'
+                }
+            )
